9cd8544fef74d3804ca60563595ab1d319d278b1
adrishyantee <adrishyantee@gmail.com>
2021-06-09 20:30:30 +0530

    added login feature

--- app/src/main/java/com/example/logcathealth/LogInActivity.java
+++ app/src/main/java/com/example/logcathealth/LogInActivity.java
@@ -1,34 +1,75 @@
 package com.example.logcathealth;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.logcathealth.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
 
 public class LogInActivity extends AppCompatActivity {
-    EditText username, loginpass;
+    EditText email, loginpass;
     Button loginsub;
+    FirebaseFirestore db;
+    String TAG = "Login";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
 
-        username=findViewById(R.id.username);
+        email=findViewById(R.id.email);
         loginpass=findViewById(R.id.loginpass);
         loginsub=findViewById(R.id.loginsub);
+        db = FirebaseFirestore.getInstance();
 
         //Button action
         loginsub.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Condition check
-                //next activity
+
+                String emaildata=email.getText().toString();
+                String pass=loginpass.getText().toString();
+                        db.collection("users")
+                                .whereEqualTo("email",emaildata)
+                                .get()
+                                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                    @Override
+                                    public void onComplete(@NonNull @NotNull Task<QuerySnapshot> task) {
+
+                                        if (task.isSuccessful()){
+                                            for (QueryDocumentSnapshot document : Objects.requireNonNull(task.getResult())) {
+                                               String password = document.get("password").toString();
+                                               if(password.equals(pass)) {
+                                                   Intent intent = new Intent(LogInActivity.this, Demo.class);
+                                                   startActivity(intent);
+                                                   break;
+                                               }
+                                                Toast.makeText(LogInActivity.this,"Retry",Toast.LENGTH_LONG).show();
+                                            }
+                                        }else {
+                                            Toast.makeText(LogInActivity.this,"Retry",Toast.LENGTH_LONG).show();
+                                            return;
+                                        }
+
+                                    }
+                                });
+                    }
+                });
             }
-        });
-    }
-}
+        }
